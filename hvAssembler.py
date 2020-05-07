17262a3eace2127db51a1a58c4f663e7ac4b54ba
Salvatore Camiolo <salvo@Salvatores-Mac-mini.local>
2020-05-07 16:15:33 +0100

    Added output

--- hvAssembler.py
+++ hvAssembler.py
@@ -20,12 +20,21 @@ else:
 hvg = ['RL12','RL13','RL5A','RL6','UL11','UL120','UL139','UL146','UL1','UL20','UL73','UL74','UL9']
 
 for gene in hvg:
-	os.system(condaDir+"/bin/bowtie2-build -q ./elongedCDS/"+gene+"_elongedCDS.fasta reference")
-	os.system(condaDir+"/bin/bowtie2 --very-sensitive-local -1 "+read1+" -2 "+read2+" -x reference -S alignment.sam -a -p "+threads)
+	print("Indexing reference for gene %s" %gene)
+	os.system(condaDir+"/bin/bowtie2-build -q ./elongedCDS/"+gene+"_elongedCDS.fasta reference > null 2>&1")
+	print("Aligning reads to reference....")
+	os.system(condaDir+"/bin/bowtie2 --very-sensitive-local -1 "+read1+" -2 "+read2+" -x reference -S alignment.sam -a -p "+threads+" > null 2>&1")
+	print("Coverting sam to bam....")
+	os.system(condaDir+"/bin/samtools view -h -bS alignment.sam > alignment.bam")
+	print("Collecting first mapped reads....")
 	os.system(condaDir+"/bin/samtools view -h -b -F 4 -f 8 alignment.bam > firstMapped.bam")
+	print("Collecting second mapped reads....")
 	os.system(condaDir+"/bin/samtools view -h -b -F 8 -f 4 alignment.bam > secondMapped.bam")
+	print("Collecting both mapped reads....")
 	os.system(condaDir+"/bin/samtools view -h -b -F12 alignment.bam > bothMapped.bam")
+	print("Merging alignments....")
 	os.system(condaDir+"/bin/samtools merge merged.bam firstMapped.bam secondMapped.bam bothMapped.bam")
+	print("Extracting reads....")
 	os.system(condaDir+"/bin/bamToFastq -i merged.bam -fq read_1.fastq -fq2 read_2.fastq")
 
 	print("finished")
